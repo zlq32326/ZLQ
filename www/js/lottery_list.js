dea704e8405fde58a98c9437c5cb6f69245c2bec
zlq <zlq@ppBand.(none)>
2015-12-14 12:45:27 -0500

    update

--- www/js/lottery_list.js
+++ www/js/lottery_list.js
@@ -7,7 +7,7 @@ angular.module('lotteryApp', []).controller('lotteryCtrl', function($scope, $htt
     for (i$ = 0, len$ = (ref$ = response.result.lottery_list).length; i$ < len$; ++i$) {
       list = ref$[i$];
       /*console.log list.lottery_name + '</a>'*/
-      list.lottery_url = '<a href="http://127.0.0.1:8080/shenren_party_tools/borad_rank.html' + '?deduplication=true' + '&lottery_id=' + list.lottery_id + '&fpage=1' + '"target="_self">' + list.lottery_name + '</a>';
+      list.lottery_url = '<a href="http://127.0.0.1:8080/family_op_tools/borad_rank.html' + '?deduplication=true' + '&lottery_id=' + list.lottery_id + '&fpage=1' + '"target="_self">' + list.lottery_name + '</a>';
       console.log(list.lottery_url);
     }
     return $('#keywords').dynatable({
